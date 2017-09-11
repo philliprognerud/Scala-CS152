1737eb162818b503b1843dff7a4c9289ca1f13ae
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-09-11 02:12:22 +0000

    homework1

--- hw1/strings.scala
+++ hw1/strings.scala
@@ -1,7 +1,9 @@
 object strings extends App {
     //++++++++++++++++++++++++
     //problem 1
-    def isPal(input: String) = {
+    def isPal(str: String) = {
+        var input = str.trim
+        input = input.toLowerCase
         var start = 0
         var end = input.length() - 1
         var isPal = true
@@ -18,11 +20,71 @@ object strings extends App {
     }
     
     //tests
-    println(isPal("racecar"))
-    println(isPal("test"))
-    println(isPal("mom"))
+    println(isPal("   raCEcar"))
+    println(isPal("   25%#%52  "))
+    println(isPal("mOM    "))
     println()
     
     //++++++++++++++++++++++++
     //problem 2
+    def filterString(str: String) = {
+        var input = str.replaceAll("""[\p{Punct}]""", "")
+        input = input.replaceAll("\\s", "")
+        input = input.toLowerCase
+        isPal(input)
+    }
+    
+    
+    //tests
+    println(filterString("   rac ,e car    "))
+    println(filterString("   25% # %52 !!! "))
+    println(filterString(".....mOM    "))
+    println()
+    
+    //++++++++++++++++++++++++
+    //problem 4
+    import scala.util.Random
+    def mkWord(value: Int = 8) = {
+        var buf = new StringBuilder
+        var counter = 0
+        while(counter < value){
+            buf += Random.alphanumeric.filter(_.isLetter).head
+            counter += 1
+        }
+        buf.toString.toLowerCase
+    }
+    
+    //tests
+    println(mkWord())
+    println(mkWord(20))
+    println(mkWord(4))
+    println()
+    
+    //++++++++++++++++++++++++
+    //problem 5
+    import scala.util.Random
+    def mkSentence(value: Int = 8) = {
+        var buf = new StringBuilder
+        
+        for(_ <- 1 to value){
+            var counter = 0
+            val rand = Random.nextInt( (10 - 1) + 1 )
+            while(counter < rand){
+                buf += Random.alphanumeric.filter(_.isLetter).head
+                counter += 1
+            }
+            buf.insert(buf.length(), " ")
+        }
+        
+        var sent = buf.toString.toLowerCase.trim
+        sent = sent.substring(0, 1).toUpperCase() + sent.substring(1)
+        sent
+    }
+    
+    //tests
+    println(mkSentence())
+    println(mkSentence(3))
+    println(mkSentence(5))
+    println()
+    
 }
