436d91b47b39b90db8ac05b083cb8b3a3b093386
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-09-28 03:41:25 +0000

    recursion finsihed

--- recursion/recursion.scala
+++ recursion/recursion.scala
@@ -45,7 +45,6 @@ object recursion extends App {
         if(isZero(m)) 1
         else if(m > 1) 2 * exp2(dec(m))
         else m*2
-        //why does returning 'm' give the value of call stack?
     
     
     //TEST
@@ -72,6 +71,11 @@ object recursion extends App {
     println(hyperExp(5))
     println()
     
+    // OUPUT
+    // 4
+    // 256
+    // 65536
+    
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //problem 5
     import scala.annotation.tailrec
@@ -83,6 +87,66 @@ object recursion extends App {
         iter(n, m)
     }
         
+    def mulWithTail(n: Int, m: Int, acc: Int): Int =
+      if (m == 0) acc
+      else mulWithTail(n, dec(m), n + acc)
     
-    println(addWithTail(5, 5))
+    def exp2WithTail(m: Int, acc: Int):Int = {
+        if(isZero(m)) 1
+        else if(m == 1) acc*2
+        else exp2WithTail(dec(m), acc*2)
+
+    }
+    
+    //TEST
+    println(addWithTail(2, 7))
+    println(mulWithTail(11, 2, 0))
+    println(exp2WithTail(6, 1))
+    println()
+    
+    //OUTPUT
+    // 9
+    // 22
+    // 64
+
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //problem 9
+    
+    //None tail recursive solution
+    def fib1(n: Int): Int = n match {
+       case 0 | 1 => n
+       case _ => fib1(n-1) + fib1(n-2)
+    }
+    
+    //Tail recursive solution
+    def fib2(n: Int): Int = { 
+      def fib_tail(n: Int, a:Int, b:Int): Int = n match {
+        case 0 => a 
+        case _ => fib_tail(n-1, b, a+b)
+      }
+      fib_tail(n, 0, 1)
+    }
+    
+     //TEST
+    println(fib1(7))
+    println(fib2(7))
+    println(fib1(4))
+    println(fib2(4))
+    println()
+    
+    //OUTPUT
+    // 13
+    // 13
+    // 3
+    // 3
+    
+    
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //problem 10
+    def choose(n: Int, k: Int):Int = {
+        if(k == 0) 1
+        if(n == k) 1
+        else choose(n-1, k-1) + choose(n-1, k)
+    }
+    
 }
